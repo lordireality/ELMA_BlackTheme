0f069f3ebba59b5baa5b2b75f400326d6151a978
lordireality <lordireality@gmail.com>
2025-04-28 12:44:23 +0300

    Исправлена переподписка инъектора на изменения

--- content.js
+++ content.js
@@ -11,9 +11,15 @@
     if (editorExists && !lastEditorVisible) {
       lastEditorVisible = true;
       const script = document.createElement("script");
+      script.id = "elmaThemeInjectJs"
       script.src = chrome.runtime.getURL("inject.js");
       script.onload = function () { this.remove(); };
       (document.head || document.documentElement).appendChild(script);
+    }
+    if(!editorExists && lastEditorVisible){
+      lastEditorVisible = false;
+      const existing = document.getElementById("elmaThemeInjectJs");
+      if (existing) existing.remove();
     }
   });
   observer.observe(document.body, {
